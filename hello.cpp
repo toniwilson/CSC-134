e435f4a2e8fbd9b4c93921c887e2fefd63529bb8
toniwilson <willson.allexandriia09@gmail.com>
2025-01-15 16:17:38 +0000

    it works

---
+++ hello.cpp
@@ -0,0 +1,15 @@
+// csc 134
+// Hello World
+// toni wilson
+// 1/15/25
+
+// #include lets us use input and output
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello, CSC 134!" << endl;
+    cout << "Codespaces get easier everytime you use them. (imo)";
+    return 0;
+}
