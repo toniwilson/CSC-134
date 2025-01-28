50b9bc3068dab3e535844ec4dafd33db0e040d53
toniwilson <willson.allexandriia09@gmail.com>
2025-01-28 21:55:32 +0000

    m1t2

---
+++ M1T2_Wilson.cpp
@@ -0,0 +1,16 @@
+//  CSC 142
+// M1T2 - Hello Student
+// Toni Wilson
+// 01/28/2025
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Toni Wilson" << endl;
+    cout << "Computer Programming" << endl;
+    cout << "I currently work as a barista and even though I'm studying to be a programmer, I wish to be a bartender later on." << endl;
+    return 0;
+}
