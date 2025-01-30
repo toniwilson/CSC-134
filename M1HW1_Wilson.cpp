14d450ed4c83ac7deb27b1f5f7f90959cdc07d59
toniwilson <willson.allexandriia09@gmail.com>
2025-01-30 22:08:49 +0000

    movie

---
+++ M1HW1_Wilson.cpp
@@ -0,0 +1,25 @@
+// CSC 134
+// M1HW1 - Movie Talk
+// 01-30-2025
+// 
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Variables
+    string name = "My Neighbor Totoro";
+    int year = 1990;
+    double box_office = 30.33; 
+
+    cout << "The movie " << name << " came out in " << year << ", and had a worldwide gross of around " << box_office << " million dollars." << endl;
+    cout << endl;
+    cout << "A fun fact about this classic film is that the idea was initially rejected." << endl;
+    cout << "As widely loved as it is by Studio Ghibli fans, it was a box office flop." << endl;
+    cout << "Fans noticed that there were distinct similarities to Alice's Adventures in Wonderland." << endl;
+    cout << "A famous quote from the movie is \"If we all keep smiling, the sprites may gradually go away and leave this place alone. Yes, I'm sure that somewhere up in that ceiling, they're busily discussing plans for leaving the house.\" by Kanta's Grandmother." << endl;
+
+    cout << endl;
+    return 0;
+}
