8dca84a6bb0a530e34c9e37424892668da2bfa08
toniwilson <willson.allexandriia09@gmail.com>
2025-02-17 16:19:56 +0000

    if

--- M3T1_Wilson.cpp
+++ M3T1_Wilson.cpp
@@ -1,8 +1,9 @@
 // CSC 134
-// M3T1
+// M3T1 and M3T2
 // Toni Wilson
 // 02/17/2025
 // ask user for the width and legth of two rectangles
+// which is bigger
 
 #include <iostream>
 using namespace std;
@@ -30,6 +31,20 @@ int main()
     // print the area
     cout << "Area 1: " << area1 << endl;
     cout << "Area 2: " << area2 << endl;
+
+    // which is larger?
+    if (area1 > area2)
+    {
+        cout << "The first one is larger." << endl;
+    }
+    else if (area2 > area1)
+    {
+        cout << "The second one is larger." << endl;
+    }
+    else
+    {
+        cout << "Neither is larger. They're both the same size." << endl;
+    }
 
     return 0;
 }
