c6b07c940b0fbbbea694623ae64160448b3158ba
toniwilson <willson.allexandriia09@gmail.com>
2025-03-10 14:47:11 +0000

    bool

--- m4ti_Wilson.cpp
+++ m4ti_Wilson.cpp
@@ -37,5 +37,30 @@ int main()
         num = num + 1;
     }
 
+    // OPTIONAL EXAMPLE
+    bool is_valid = false;
+    int choice;
+    while (is_valid == false) 
+    {
+        cout << "Press 1 or 2 to finish." << endl;
+        cin >> choice;
+        if (choice == 1) 
+        {
+            cout << "You picked 1" << endl;
+            is_valid = true;
+        }
+        else if (choice == 2) 
+        {
+            cout << "You picked 2" << endl;
+            is_valid = true;
+        }
+        else 
+        {
+            cout << "Sorry, not valid. Try again." << endl;
+        }
+    }
+
+    cout << "Goodbye." << endl;
+    
     return 0;
 }
