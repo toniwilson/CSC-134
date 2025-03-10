81385c3b16cfcd6d20a3a7c1510499530c654468
toniwilson <willson.allexandriia09@gmail.com>
2025-03-10 14:17:37 +0000

    m4t1 pt1

---
+++ m4ti_Wilson.cpp
@@ -0,0 +1,23 @@
+// CSC 134
+// M4T1
+// Toni Wilson
+// 03/10/2025
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // PART ONE, JUST SAY "HI" 5 TIMES
+    int count = 1;
+    while (count <= 5)
+    {
+        cout << "Hello number " << count << endl;
+        // count = count + 1;
+        count++; // same as above
+    }
+    cout << "Done!" << endl;
+
+    // PART TWO
+    return 0;
+}
