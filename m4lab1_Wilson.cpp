6bcd22def324674a8e9c24c0bc4a4f20573030aa
toniwilson <willson.allexandriia09@gmail.com>
2025-03-17 14:55:46 +0000

    box

--- m4lab1_Wilson.cpp
+++ m4lab1_Wilson.cpp
@@ -10,6 +10,8 @@ using namespace std;
 int main () 
 {
     string pixel = "💙"; 
+    const int WIDTH = 7;
+    const int HEIGHT = 7;
     // cout << pixel << endl;
 
     // three ways to run a loop
@@ -34,9 +36,27 @@ int main ()
     // this is the one time using i as a variable is ok
     // i stands for index
     cout << endl;
-    for (int i=0; i < 10; i++)
+    cout << pixel << " printed horizontally (row)" << endl;
+    for (int i=0; i < WIDTH; i++)
     {
         cout << pixel << " ";
     }
     cout << endl;
+
+    cout << pixel << " printed vertically (column)" << endl;
+    for (int i=0; i < HEIGHT; i++)
+    {
+        cout << pixel << endl;
+    }
+    cout << endl << "Printing a box " << WIDTH << " by " << HEIGHT << endl;
+    // THIS IS A NESTED LOOP
+    // for each row
+    for (int h = 0; h < HEIGHT; h++) {
+        // print the current row
+        for (int w = 0; w < WIDTH; w++) {
+            cout << pixel << " ";
+        }
+        // go to the next line
+        cout << endl;
+    }
 }
